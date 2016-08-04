6bb9c58a8f06a1a5e4f5108dbc9d77f4699df1ce
FYpanda <yzjxiaojiansha@gmail.com>
2016-08-04 23:09:39 +0800

    Update Utorrent-64.sh

--- Utorrent-64.sh
+++ Utorrent-64.sh
@@ -3,4 +3,4 @@
 wget http://download.ap.bittorrent.com/track/beta/endpoint/utserver/os/linux-x64-ubuntu-13-04
 tar -zxvf linux-x64-ubuntu-13-04
 cd utorrent-server-alpha-v3_3
- ./utserver
+./utserver
